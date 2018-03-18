eb807d9b6d2a3c2ddfaec861a5708db64b97e117
Tirz <tirz@localhost.localdomain>
2018-03-18 04:52:39 +0100

    fix const and size_t - strstr

--- i_src/i_strstr.c
+++ i_src/i_strstr.c
@@ -6,19 +6,21 @@
  *
  */
 
+#include "libi.h"
+
 char		*i_strstr(const char *str, const char *token)
 {
   size_t	i = 0;
   size_t	j;
 
   if (!*token)
-    return (str);
+    return ((char*)str);
   while (str[i])
     {
       j = 0;
       while (token[j] == str[i + j])
 	if (!token[++j])
-	  return (&str[i]);
+	  return ((char*)&str[i]);
       i += j + 1;
     }
   return (0);
